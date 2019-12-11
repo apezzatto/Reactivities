3ffb09774607166ff0e61fdbef72abfd20381096
apezzatto <rogerio.meneghelo@gmail.com>
2019-12-11 15:15:01 -0500

    Isolating API calls, loading indicator, code refactoring

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import { Item, Button, Label, Segment } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 
 interface IProps {
     activities: IActivity[];
     selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
+    deleteActivity: (event: SyntheticEvent<HTMLButtonElement> ,id: string) => void;
+    submitting: boolean;
+    target: string
 }
 
-export const ActivityList: React.FC<IProps> = ({activities, selectActivity, deleteActivity}) => {
+export const ActivityList: React.FC<IProps> = ({activities, selectActivity, deleteActivity, submitting, target}) => {
     return (
         <Segment clearing>
             <Item.Group divided>
@@ -29,7 +31,9 @@ export const ActivityList: React.FC<IProps> = ({activities, selectActivity, dele
                                     color='blue'
                                 />
                                 <Button 
-                                    onClick={() => deleteActivity(activity.id)} 
+                                    name={activity.id}
+                                    onClick={(e) => deleteActivity(e, activity.id)} 
+                                    loading={target === activity.id && submitting}
                                     floated='right' 
                                     content='Delete' 
                                     color='red'
