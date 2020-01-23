92ce0f786a56c74a7693f79eee11c9d7910c6a19
apezzatto <rogerio.meneghelo@gmail.com>
2020-01-22 23:12:13 -0500

    Login and Register features finished

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect } from 'react'
-import ActivityStore from '../../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
 import { RouteComponentProps } from 'react-router';
 import { LoadingComponent } from '../../../app/layout/LoadingComponent';
@@ -8,14 +7,15 @@ import ActivityDetailInfo from './ActivityDetailInfo';
 import ActivityDetailChat from './ActivityDetailChat';
 import ActivityDetailSidebar from './ActivityDetailSidebar';
 import { Grid } from 'semantic-ui-react';
+import { RootStoreContext } from '../../../app/stores/rootStore';
 
 interface DetailParams {
   id: string
 }
 
 const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({match}) => {
-  const activityStore = useContext(ActivityStore);
-  const {activity, loadActivity, loadingInitial} = activityStore;
+  const rootStore = useContext(RootStoreContext);
+  const {activity, loadActivity, loadingInitial} = rootStore.activityStore;
 
   useEffect(() => {
     loadActivity(match.params.id);
